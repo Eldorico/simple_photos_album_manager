c48acc707a44ad98de4bfab4857c476e65297d0c
Eldorico <eldorico@gmail.com>
2018-09-15 22:08:03 +0200

    webapp/created some logic to import the album's miniature's urls (didnt tested) :-/

--- webapp/src/service.js
+++ webapp/src/service.js
@@ -6,6 +6,7 @@ export default data = {
     categories : [],
     albumsSortedByCategory : {},
     allAlbums : [],
+    albumsSortedById : {},
     getCategories : function() {
         Vue.http.get('categories')
           .then(response =>{ return response.json(); })
@@ -35,10 +36,51 @@ export default data = {
               for(var i=0; i<data['albums'].length; i++){
                   this.allAlbums.push(data['albums'][i]);
                   this.albumsSortedByCategory[data['albums'][i]['category']].push(data['albums'][i]);
+                  this.albumsSortedById[data['albums'][i]['id']] = data['albums'][i];
               }
+
+              // debug
+              console.log("tous les albums:");
+              console.log(this.allAlbums);
           });
     },
-    showCategories : function(){
-        console.log(this.categories);
+    getAlbum : function(albumId){
+        if(albumId in this.albumsSortedById){
+            return this.albumsSortedById[albumId];
+        }else{
+            return -1;
+        }
+    },
+    getAlbumMiniatureUrl : function(idAlbum){
+        var album = this.getAlbum(idAlbum);
+        if(album == -1){
+            console.err("getAlbumMiniatureUrl("+idAlbum+") : album doesnt exists");
+            return;
+        }
+
+        // if there is allready a miniature in album, return the miniature.
+        if('miniatureURL' in album && album['miniatureURL'] != 'NO_PHOTO_ERROR' || album['miniatureURL'] != 'WAITING_FOR_MINIATURE'){
+            return album['miniatureURL'];
+        }
+        // if there is no photo in album, add "NO_PHOTO_ERROR" in album miniature and return
+        if(album['photos'].length == 0){
+            this.addAlbumMiniatureUrl(idAlbum, 'NO_PHOTO_ERROR');
+            return this.getAlbum(idAlbum)['miniatureURL'];
+        }
+
+        // add the miniature album url
+        Vue.http.get('images/singleImage/'+album['photos']['photoId']+'?miniature=true')
+            .then(response =>{ return response.json(); })
+            .then(data =>{
+                this.addAlbumMiniatureUrl(idAlbum, data['url']);
+            });
+
+        // return something while the miniature url is getting updated
+        this.addAlbumMiniatureUrl(idAlbum, 'WAITING_FOR_MINIATURE');
+        return this.getAlbum(idAlbum)['miniatureURL'];
+
+    },
+    addAlbumMiniatureUrl : function(idAlbum, url){
+        this.albumsSortedById[idAlbum]['miniatureURL'] = url; // TODO: check if this adds in all albums list!
     }
 }
