9d8ace991f9119cacdd5bfc4b33e46fdf53f7d41
Eldorico <eldorico@gmail.com>
2018-09-15 21:04:21 +0200

    webapp/data service : added getAllAlbumsInfo()

--- webapp/src/service.js
+++ webapp/src/service.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 let data;
 export default data = {
     categories : [],
+    albumsSortedByCategory : {},
+    allAlbums : [],
     getCategories : function() {
         Vue.http.get('categories')
           .then(response =>{ return response.json(); })
@@ -12,7 +14,29 @@ export default data = {
                   this.categories.push({'id': data['categories'][i]['id'],
                   'name': data['categories'][i]['name']});
               }
+              this.getAllAlbumsInfo();
         });
+    },
+    getAllAlbumsInfo : function(){
+        Vue.http.get('albums')
+          .then(response =>{ return response.json(); })
+          .then(data =>{
+              // sort albums by name
+              data['albums'].sort(function(el1, el2){
+                  if(el1.albumName < el2.albumName) return -1;
+                  if(el2.albumName > el2.albumName) return 1;
+                  return 0;
+              });
+
+              // create the data albums
+              for(var i=0; i<this.categories.length; i++){
+                  this.albumsSortedByCategory[this.categories[i]['id']] = [];
+              }
+              for(var i=0; i<data['albums'].length; i++){
+                  this.allAlbums.push(data['albums'][i]);
+                  this.albumsSortedByCategory[data['albums'][i]['category']].push(data['albums'][i]);
+              }
+          });
     },
     showCategories : function(){
         console.log(this.categories);
