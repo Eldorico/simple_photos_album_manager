3d65c05ad8cffe1b9a9fa734a4e02db4657d1cae
Eldorico <eldorico@gmail.com>
2018-09-17 20:20:52 +0200

    webapp/will add scoped on every vue...

--- webapp/src/PageAlbumView.vue
+++ webapp/src/PageAlbumView.vue
@@ -1,13 +1,24 @@
 <template>
 <div>
+    <div class="header">
+        <span>Retour</span>
+        <span class="album-title">{{albumTitle}}</span>
+    </div>
     PAGE ALBUM VIEW for album {{this.id}}. From category referer{{this.categoryReferer}}
     <a @click="returnToAlbumsList">go to albums list</a>
 </div>
 </template>
 
 <script>
+import Data from './service.js';
+
 export default{
     props: ['id', 'categoryReferer'],
+    data : function(){
+        return{
+            albumTitle : Data.getAlbum(this.id)['albumName']
+        }
+    },
     methods : {
         returnToAlbumsList : function(){
             this.$router.push('/albums/'+this.categoryReferer);
@@ -17,5 +28,22 @@ export default{
 </script>
 
 <style>
+    .header{
+        background-color: #315e33;
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+        overflow: hidden;
+        position: fixed;
+        top: 0;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: center; /* align horizontal */
+        align-items: center;
 
+    }
+    .album-title{
+        background-color: white;
+    }
 </style>
--- webapp/src/components/CategoriesList.vue
+++ webapp/src/components/CategoriesList.vue
@@ -28,7 +28,7 @@ export default {
 }
 </script>
 
-<style>
+<style >
 .container{
     width: 100%;
     height: 100%;
